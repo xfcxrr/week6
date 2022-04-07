5f6b80b87613cf1eb876674948b9c3ee7fa1ecf7
Chenlei <chenlei.hong@pepperdine.edu>
2022-04-07 12:22:02 +0800

    data cleaning exercise

---
+++ chapter 8.R
@@ -0,0 +1,10 @@
+pacman::p_load(
+  rio,        # importing data  
+  here,       # relative file pathways  
+  janitor,    # data cleaning and tables
+  lubridate,  # working with dates
+  epikit,     # age_categories() function
+  tidyverse   # data management and visualization
+)
+linelist_raw <- import("linelist_raw.xlsx")
+head(linelist_raw)
